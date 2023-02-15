0a06785d654c9ec1257b27fcad3739badd3463d1
Robert Hardy <robert@goattea.com>
2023-02-14 18:55:32 -0700

    Higher Order funcs

--- learning.js
+++ learning.js
@@ -3,3 +3,41 @@ let r = firstName.charAt(2)
 console.log(r)
 var window
 firstName
+
+
+let a = [
+    "apple",
+    "pear",
+    "banana",
+    "grapes"
+];
+
+// let b  =  a.filter(f => f.startsWith("g"));
+
+// a.forEach(function (fruit, i, c) {
+//     fruit = fruit + " is yummy";
+//     c[i] = fruit;
+//     console.log(fruit, i, c);
+// });
+
+// console.log(a, b);
+
+let o = [
+    { firstName: "Robert", age: 52},
+    { firstName: "Cody", age: 33},
+    { firstName: "Willow", age: 17},
+];
+
+console.log(o);
+
+o.forEach((x) => {
+    if(x.age > 50) {
+        console.log(x.firstName + " is old!");
+        x.isOld = true;
+    }
+})
+
+console.log(o)
+
+let sum = o.reduce((a, b) =>  a.age + b.age);
+console.log(sum);
